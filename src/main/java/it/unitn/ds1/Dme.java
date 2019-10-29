7c58086e38be4b9933452fe6f2ea25cab44b9844
simoberny <simone.bernabe@studenti.unitn.it>
2019-10-29 11:22:29 +0100

    Menu choice

--- src/main/java/it/unitn/ds1/Dme.java
+++ src/main/java/it/unitn/ds1/Dme.java
@@ -1,10 +1,14 @@
 package it.unitn.ds1;
 
 import akka.actor.*;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 
 import scala.concurrent.duration.Duration;
 
+import java.io.InputStreamReader;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -17,7 +21,36 @@ public class Dme{
     public static void main(String[] args){
         system = ActorSystem.create("dme");
         master = system.actorOf(Props.create(ParentNode.class));
+
+        String let = "";
+
+        do{
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                let = br.readLine();
+                System.out.println("Lettura: " + let);
+
+                switch (let){
+                    case "":    //Enter
+                        System.out.println("Termino...");
+                        system.terminate();
+                        break;
+                    case "1":    // First test
+                        System.out.println("First test running...");
+                        master.tell(new Test1(), master);
+                        break;
+                    case "2":
+                        System.out.println("Second test running...");
+                        master.tell(new Test2(), master);
+                        break;
+                }
+            }
+            catch (IOException ioe) {}
+        }while(let.length() != 0);
     }
+
+    public static class Test1 implements Serializable {}
+    public static class Test2 implements Serializable {}
 }
 
 class ParentNode extends AbstractActor{
@@ -35,8 +68,7 @@ class ParentNode extends AbstractActor{
         neighbor.add(new Integer[]{3});
 
         for(int id=0; id<neighbor.size(); id++){
-            tree.add(context().actorOf(
-                    Node.props(id, neighbor.get(id)), "node"+id));
+            tree.add(context().actorOf(Node.props(id, neighbor.get(id)), "node"+id));
         }
 
         // Ensure that no one can modify the group
@@ -51,30 +83,12 @@ class ParentNode extends AbstractActor{
         // Flood token position
         floodTokenPosition(2);
 
-        
-        getContext().getSystem().scheduler().scheduleOnce(
-                Duration.create(1, TimeUnit.SECONDS),
-                tree.get(2), new Node.Stop(), getContext().getSystem().dispatcher(), getSelf());
-        getContext().getSystem().scheduler().scheduleOnce(
-                Duration.create(3, TimeUnit.SECONDS),
-                tree.get(0), new Node.StartTokenRequest(16000), getContext().getSystem().dispatcher(), getSelf());
-        getContext().getSystem().scheduler().scheduleOnce(
-                Duration.create(5, TimeUnit.SECONDS),
-                tree.get(4), new Node.StartTokenRequest(1000), getContext().getSystem().dispatcher(), getSelf());
-       
-        getContext().getSystem().scheduler().scheduleOnce(
-                Duration.create(12, TimeUnit.SECONDS),
-                tree.get(4), new Node.StartTokenRequest(1000), getContext().getSystem().dispatcher(), getSelf());  
-        
-        try {
-            System.out.println(">>> Press ENTER to exit <<<");
-            System.in.read();
-        } 
-        catch (IOException ioe) {}
-        
-        getContext().getSystem().terminate();
+        System.out.println("-------- Select a test ---------");
+        System.out.println("1. Multiple request on node within CS");
+        System.out.println("2. Failure of a node and request to it");
+        System.out.println("---------------------------------");
     }
-    
+
     private void onNodeTerminated(Node.Terminated msg){
         getContext().getSystem().scheduler().scheduleOnce(
                 Duration.create(20, TimeUnit.SECONDS),
@@ -106,14 +120,39 @@ class ParentNode extends AbstractActor{
 
         // Token position flooded
         tree.get(node).tell(new Node.StartTokenFlood(), getSelf());
-    }
 
-    
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public Receive createReceive() {
         return receiveBuilder()
                 .match(Node.Terminated.class, this::onNodeTerminated)
+                .match(Dme.Test1.class, this::onTest1)
+                .match(Dme.Test2.class, this::onTest2)
                 .build();
+    }
+
+    private void onTest1(Dme.Test1 msg){
+        getContext().getSystem().scheduler().scheduleOnce(
+                Duration.create(1, TimeUnit.SECONDS),
+                tree.get(2), new Node.Stop(), getContext().getSystem().dispatcher(), getSelf());
+        getContext().getSystem().scheduler().scheduleOnce(
+                Duration.create(3, TimeUnit.SECONDS),
+                tree.get(0), new Node.StartTokenRequest(16000), getContext().getSystem().dispatcher(), getSelf());
+        getContext().getSystem().scheduler().scheduleOnce(
+                Duration.create(5, TimeUnit.SECONDS),
+                tree.get(4), new Node.StartTokenRequest(1000), getContext().getSystem().dispatcher(), getSelf());
+        getContext().getSystem().scheduler().scheduleOnce(
+                Duration.create(12, TimeUnit.SECONDS),
+                tree.get(4), new Node.StartTokenRequest(1000), getContext().getSystem().dispatcher(), getSelf());
+    }
+
+    private void onTest2(Dme.Test2 msg){
+
     }
 }
